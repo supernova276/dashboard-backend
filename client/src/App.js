55ad0d11d33b1ecfc59b333a872d09bf6483b7fa
Akshita <akshita.dixit2706@gmail.com>
2024-11-05 10:06:38 +0530

    minor-change

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,28 @@
 
 import { CssBaseline,ThemeProvider } from "@mui/material";
 import { createTheme } from '@mui/material/styles';
-import { create } from "@mui/material/styles/createTransitions";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { themeSettings } from "theme";
 import { BrowserRouter } from "react-router-dom";
 import  Dashboard from "scenes/dashboard/Dashboard"
 import Layout from "scenes/layout/Layout";
-import {Route, Routes} from 'react'
-import { Navigate } from "react-router-dom";
+import { Navigate,Route, Routes} from "react-router-dom";
 
-function App() {
+const App=()=> {
   const mode=useSelector((state)=>state.global.mode)
 
-  const theme=useMemo(()=>createTheme(themeSettings((mode)),[mode]))
+  const theme=useMemo(()=>createTheme(themeSettings(mode),[mode]))
 
   return <div className="app ">
     <BrowserRouter>
     <ThemeProvider theme={theme}>
       <CssBaseline/>  
       <Routes>
-        {/* <Route element={<Layout/>}> */}
+        <Route element={<Layout/>}>
         <Route path="/" element={<Navigate to ="/dashboard" replace/>}></Route>
         <Route path="/dashboard" element={<Dashboard/>}></Route>
-        {/* </Route> */}
+        </Route>
       </Routes>
     </ThemeProvider>
     </BrowserRouter>
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,6 +1,4 @@
-import { createTheme } from '@mui/material';
 import { createSlice } from '@reduxjs/toolkit';
-import { themeSettings } from 'theme';
 
 const initialState={
     mode:"dark"
@@ -13,9 +11,9 @@ export const globalThemeSlice=createSlice({
     reducers:{
         setMode:(state,param)=>{
             state.mode=state.mode==='light'? 'dark':'dark'
-        }
+        },
 
-        }
+        },
     })
 
     export const {setMode}=globalThemeSlice.actions
